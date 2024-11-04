c8f0ef653aac7a5145be753f406158112aaa70e4
redjax <jackenyon@gmail.com>
2024-11-04 00:07:15 -0500

    Convert tasks to shared celery tasks

--- src/auto_weather/celeryapp/tasks/adhoc/demo/tasks.py
+++ src/auto_weather/celeryapp/tasks/adhoc/demo/tasks.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+from celery import shared_task
 from auto_weather.celeryapp.celery_main import celery_app
 
 from loguru import logger as log
 
-@celery_app.task(name="say_hello")
+
+# @celery_app.task(name="say_hello")
+@shared_task(name="say_hello")
 def task_say_hello(name: str = "world"):
     log.debug(f"Name: {name}")
 
